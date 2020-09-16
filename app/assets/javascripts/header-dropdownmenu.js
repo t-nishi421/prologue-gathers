cef9a917ed6b0912b282fee09ab1f25811054693
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-09-16 15:28:49 +0900

    ヘッダーのレイアウトを改修

---
+++ app/assets/javascripts/header-dropdownmenu.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('.user__box__dropdown__arrow').hover(function(){
+    $('.user__box__dropdown__arrow-menu').css('display', 'block');
+  },function(){
+    $('.user__box__dropdown__arrow-menu').css('display', 'none');
+  });
+
+  $('.user__box__dropdown__arrow-menu').hover(function(){
+    $('.user__box__dropdown__arrow-menu').css('display', 'block');
+  },function(){
+    $('.user__box__dropdown__arrow-menu').css('display', 'none');
+  });
+});
