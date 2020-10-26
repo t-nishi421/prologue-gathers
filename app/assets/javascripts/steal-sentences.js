81e51eb9e00fbd67beb9a57c825dbe0bf121583a
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-26 15:48:37 +0900

    保存するテキストを取得するjsを実装

--- app/assets/javascripts/steal-sentences.js
+++ app/assets/javascripts/steal-sentences.js
@@ -1,7 +1,27 @@
 $(function() {
   
   /*** 変数 ***/
+  // 手動設定
   const sentencesArea = '.story-content__text';
+  const addStealSentencesArea = '.StealContents__stealSentences__sentences';
+  const saveButton = '.SentenceBox__saveButton';
+  const sentence = '.sentence';
+
+  // 自動設定
+  let indexNumber = 0;
+
+  let stealSentence = function(sentence, thisIndex) {
+    const html = 
+    `<div class="SentenceBox">
+      <div class="SentenceBox__saveButton" data-index="${thisIndex}">
+        <i class="fa fa-save"></i>
+      </div>
+      <div class="SentenceBox__sentence sentence${thisIndex}">
+        ${sentence}
+      </div>
+    </div>`;
+    return html;
+  }
   
   // ドラッグした文章を取得
   $(sentencesArea).on('mouseup', function(e){  //mouseupでイベント発火
@@ -10,7 +30,16 @@ $(function() {
       selectedStr = window.getSelection().toString();  //文章取得
       if(selectedStr !== '' && selectedStr !== '\n'){  //文章チェック
         console.log(selectedStr);
+        $(addStealSentencesArea).append(stealSentence(selectedStr, indexNumber));
+        indexNumber++;
       }
     }
+  });
+
+  // セーブ
+  $(saveButton).on('click', function(){
+    const targetIndex = $(this).data('index');
+    const text = $(sentence + targetIndex).text();
+    console.log(text); // 保存するテキスト
   });
 });
