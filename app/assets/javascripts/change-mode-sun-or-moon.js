a1907cb06d05d52f48bd45747d9574583485ca02
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-10-26 13:06:22 +0900

    モードのセッション保持を実装

--- app/assets/javascripts/change-mode-sun-or-moon.js
+++ app/assets/javascripts/change-mode-sun-or-moon.js
@@ -1,27 +1,51 @@
 $(function() {
   /*** 変数 ***/
-  const modeChangeButton = '.stealSwitch'
-  const sunArea = '.stealSwitch__sun'
-  const moonArea = '.stealSwitch__moon'
-  const wrapper =  '.wrapper'
+  // 手動設定
+  const modeChangeButton = '.stealSwitch';
+  const sunArea = '.stealSwitch__sun';
+  const moonArea = '.stealSwitch__moon';
+  const wrapper =  '.wrapper';
 
   // 自動設定
-  let changeModeEventNow = false
-  let modeSunOrMoon = true
+  let modeSunOrMoon = true; // 太陽:true, 月:false
 
+  /*** 関数 ***/
+  function addModeToSession(){
+    sessionStorage.setItem('mode', modeSunOrMoon);
+  }
+
+  function sunMode() {
+    modeSunOrMoon = true;
+    $(sunArea).css('opacity', '1');
+    $(moonArea).css('opacity', '0.2');
+    $(wrapper).css('background-color', "rgba(0, 50, 100, 0.2)");
+    addModeToSession();
+  }
+  
+  function moonMode() {
+    modeSunOrMoon = false;
+    $(sunArea).css('opacity', '0.2');
+    $(moonArea).css('opacity', '1');
+    $(wrapper).css('background-color', "rgba(0, 25, 50, 0.7)");
+    addModeToSession();
+  }
+  
   /*** イベント ***/
+  $(document).ready(function(){
+    var session = sessionStorage.getItem('mode');
+    console.log(session);
+    if (session == "true") {
+      sunMode();
+    } else {
+      moonMode();
+    }
+  });    
 
   $(modeChangeButton).on('click',function(){
     if(modeSunOrMoon) { // moonモードに変更
-      modeSunOrMoon = false
-      $(sunArea).css('opacity', '0.2');
-      $(moonArea).css('opacity', '1');
-      $(wrapper).css('background-color', "rgba(0, 25, 50, 0.7)")
+      moonMode();
     } else { // sunモードに変更
-      modeSunOrMoon = true
-      $(sunArea).css('opacity', '1');
-      $(moonArea).css('opacity', '0.2');
-      $(wrapper).css('background-color', "rgba(0, 50, 100, 0.2)")
+      sunMode();
     }
   });
 });
