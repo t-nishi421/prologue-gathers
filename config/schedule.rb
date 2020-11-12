39e11b3c106aec3800e63c027132334238d13ba8
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-11-12 09:23:30 +0900

    Batch処理を編集

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,9 +23,8 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 env :PATH, ENV['PATH']
 set :environment, Rails.env.to_sym
-# set :environment, :production
 set :output, "#{Rails.root.to_s}/log/cron.log"
-set :job_template, "/bin/zsh -l -c ':job'"
+set :job_template, "bash -l -c ':job'"
 job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 # every 1.day, :at => '5:00 am' do
