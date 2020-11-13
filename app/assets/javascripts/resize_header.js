b7620ce6c2259bb8215cae60a5280aeb75112442
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-11-13 09:31:25 +0900

    ヘッダーのリサイズを実装

--- app/assets/javascripts/resize_header.js
+++ app/assets/javascripts/resize_header.js
@@ -1,20 +1,34 @@
-// $(function(){
+$(function(){
 
-//   // 【マイページ】サイドバーのリサイズ
-//   $(window).on('load resize', function(){
-//     const MyPageWide = '.MyPageWide';
-//     const MyPageSmall = '.MyPageSmall';
+  // 【マイページ】サイドバーのリサイズ
+  $(window).on('load resize', function(){
+    const WideContent = '.WideContent';
+    const SmallContent = '.SmallContent';
     
-//     var winW = $(window).width();
-//     var devW = 1124;
-//     if (winW <= devW) {
-//       //devWpx以下の時の処理
-//       $(MyPageWide).css('display', 'none');
-//       $(MyPageSmall).css('display', 'block');
-//     } else {
-//       //devWpxより大きい時の処理
-//       $(MyPageSmall).css('display', 'none');
-//       $(MyPageWide).css('display', 'block');
-//     }
-//   });
-// });
+    var winW = $(window).width();
+    var devW = 1090;
+    if (winW <= devW) {
+      //devWpx以下の時の処理
+      $(WideContent).css('display', 'none');
+      $(SmallContent).css('display', 'block');
+    } else {
+      //devWpxより大きい時の処理
+      $(SmallContent).css('display', 'none');
+      $(WideContent).css('display', 'block');
+    }
+  });
+
+  $(window).on('load resize', function(){
+    const VerySmallNone = '.VerySmallNone';
+    
+    var winW = $(window).width();
+    var devW = 720;
+    if (winW <= devW) {
+      //devWpx以下の時の処理
+      $(VerySmallNone).css('display', 'none');
+    } else {
+      //devWpxより大きい時の処理
+      $(VerySmallNone).css('display', 'block');
+    }
+  });
+});
