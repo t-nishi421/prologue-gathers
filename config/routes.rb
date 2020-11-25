a193aa51eff6c74fd1e159e22c8adb9cb2e57fdb
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-11-25 16:53:09 +0900

    確認用ルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -28,8 +28,4 @@ Rails.application.routes.draw do
   end
 
   resources :texts, only: [:update], defaults: { fomat: 'json'}
-
-  get '404', to: 'application#render_404'
-  get '422', to: 'application#render_422'
-  get '500', to: 'application#render_500'
 end
