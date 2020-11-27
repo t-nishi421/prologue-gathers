0a057324dc5d10638a1c3098765bb69326f9279a
t-nishi421 <69189773+t-nishi421@users.noreply.github.com>
2020-11-27 15:50:18 +0900

    検索バーの不具合を修正

--- app/assets/javascripts/resize_header.js
+++ app/assets/javascripts/resize_header.js
@@ -5,20 +5,26 @@ $(function(){
     const WideContent = '.WideContent';
     const SmallContent = '.SmallContent';
     const SmallContentSpecial = '.SmallContent.Special';
+    const SearchItems = '.SearchItems';
+    const SearchBar = '.SearchBar';
     
     var winW = $(window).width();
     var devW = 1090;
     if (winW <= devW) {
       //devWpx以下の時の処理
+      
       $(WideContent).css('display', 'none');
       if($('.center').length) {
         //classが存在したら実行する
+        $(SearchBar).appendTo(SmallContentSpecial);
         $(SmallContentSpecial).css('display', 'block');
       } else {
+        $(SearchBar).appendTo(SmallContent + SearchItems);
         $(SmallContent).css('display', 'block');
       }
     } else {
       //devWpxより大きい時の処理
+      $(SearchBar).appendTo(WideContent + SearchItems);
       $(WideContent).css('display', 'block');
       if($('.center').length) {
         //classが存在したら実行する
